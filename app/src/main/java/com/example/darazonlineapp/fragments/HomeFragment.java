756b6e4ffaf3936fa888b833c5b78eecd7460f7a
bihwal <strength147@gmail.com>
2020-01-10 09:37:45 +0545

    Fragments

---
+++ app/src/main/java/com/example/darazonlineapp/fragments/HomeFragment.java
@@ -0,0 +1,61 @@
+package com.example.darazonlineapp.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.darazonlineapp.R;
+
+import java.util.ArrayList;
+
+import com.example.darazonlineapp.models.ProductsAdapter;
+import com.example.darazonlineapp.models.Products;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    View view;
+    private RecyclerView recyclerView;
+    public List<Products> productsList;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       // return inflater.inflate(R.layout.fragment_cart, container, false);
+
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        productsList=new ArrayList<>();
+        //productsList.add(new Products("Ram","5000",R.drawable.ram));
+        //productsList.add(new Products("Speaker","5000",R.drawable.ram));
+        //productsList.add(new Products("Speaker","5000",R.drawable.ram));
+        //productsList.add(new Products("Speaker","5000",R.drawable.ram));
+
+        recyclerView=view.findViewById(R.id.recyclerView);
+
+        ProductsAdapter productsAdapter=new ProductsAdapter(getContext(),productsList);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        recyclerView.setAdapter(productsAdapter);
+
+        return view;
+
+    }
+
+}
--- app/src/main/java/com/example/darazonlineapp/models/ProductsAdapter.java
+++ app/src/main/java/com/example/darazonlineapp/models/ProductsAdapter.java
@@ -14,59 +14,57 @@ import com.example.darazonlineapp.models.Products;
 import com.squareup.picasso.Picasso;
 import java.util.List;
 
-public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.MyViewHolder>{
+public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ProductsViewHolder> {
 
-    Context context;
-    List<Products> Productss;
+    Context mContext;
+    List<Products>productsList;
 
-    public ProductsAdapter(Context context, List<Products> Productss) {
-        this.context = context;
-        this.Productss = Productss;
+    public ProductsAdapter(Context mContext, List<Products> productsList) {
+        this.mContext = mContext;
+        this.productsList = productsList;
     }
+
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_product_recyclerview,parent,false);
+    public ProductsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_dashboard,parent,false);
 
-        return new MyViewHolder(view);
+        return new ProductsViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
-        //// getting data according to position
-        final Products Products = Productss.get(position);
-
-        holder.txt_item_product_name.setText(Products.getProduct_name());
-        holder.txt_item_product_price.setText(Products.getProduct_price());
-        // holder.item_product_image.setTe(Products.getProduct_image());
-        // get Product Image
-        // String img
+    public void onBindViewHolder(@NonNull ProductsViewHolder holder, int position) {
+        Products products=productsList.get(position);
+        holder.tvProductName.setText(products.getProduct_name());
+        holder.tvPrice.setText(products.getProduct_price());
         Picasso.get()
-                .load("http://www.piyushp.com.np/sport_fanatic/api/member/image/daraz_image/product/"+Products.getProduct_image())
+                .load("http://www.piyushp.com.np/sport_fanatic/api/member/image/daraz_image/product/"+products.getProduct_image())
                 .placeholder(R.drawable.ic_launcher_background)
                 .resize(220, 220)
                 .centerCrop()
-                .into(holder.item_product_image);
+                .into(holder.productImage);
+        //  holder.productImage.setImageResource(products.getImageId());
     }
 
     @Override
     public int getItemCount() {
-        return Productss.size();
+        return productsList.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
+    public class ProductsViewHolder extends RecyclerView.ViewHolder {
 
-        TextView txt_item_product_name,txt_item_product_price;
-        ImageView item_product_image;
-
-        public MyViewHolder(View itemView) {
+        ImageView productImage;
+        TextView tvProductName,tvPrice;
+        public ProductsViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            txt_item_product_name = itemView.findViewById(R.id.txtProductName);
-            txt_item_product_price = itemView.findViewById(R.id.txtPrice);
-            item_product_image = itemView.findViewById(R.id.imgProduct);
+            productImage=itemView.findViewById(R.id.productImage);
+
+            tvProductName=itemView.findViewById(R.id.tvProductName);
+            tvPrice=itemView.findViewById(R.id.tvPrice);
+
         }
     }
 }
